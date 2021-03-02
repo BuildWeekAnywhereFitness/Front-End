94f7750da7975c8f6251436a377c1127810f301c
Fabian-Gorka <fabian-gorka@lambdastudents.com>
2021-03-01 18:51:50 -0600

    added Schema

--- front-end/src/components/formSchema.js
+++ front-end/src/components/formSchema.js
@@ -2,16 +2,14 @@ import * as yup from 'yup'
  
 const formSchema = yup.object().shape({
     username: yup.string()
-        .trim()
         .required('Username is required!')
         .min(3, 'Username must be at least 3 characters long'),
     password: yup.string()
-        .trim()
         .required('Must enter a password!')
         .min(8, 'Password must be at least 8 characters long'),
    
-    client: yup.boolean(),
-    instructor: yup.boolean()
+    user: yup.string()
+    .oneOf(['client', 'instructor'], "click a button")
 })
  
 export default formSchema 
