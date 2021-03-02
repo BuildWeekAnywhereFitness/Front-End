1f5def0c99a4b43029ab52397ce184806858a1ef
D'Artagnan von Pingel <dvonpingel@gmail.com>
2021-03-01 18:04:17 -0700

    First Commit on branch - basic styling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
+
 import ClientWalkthrough from "./components/ClientWalkthrough";
 import InstructorWalkthrough from "./components/InstructorWalkthrough";
+import ClientDash from "./components/ClientDash";
 
 function App() {
-  
   return (
     <div>
-      <ClientWalkthrough />
+      <ClientDash />
     </div>
   );
 }
